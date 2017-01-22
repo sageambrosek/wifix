8e9492a5f07abf7e0d615caa9ca35a909200c61c
sageambrosek <sageisabeast@gmail.com>
2017-01-21 17:43:40 -0700

    Create wifix.py

---
+++ wifix.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import os
+response = os.system('ping 8.8.8.8 -c 1')
+if response == 0:
+	print 'exito'
+else:
+	print 'restarting network manager...'
+	os.system('sudo service network-manager restart')
+	print "restart complete"
