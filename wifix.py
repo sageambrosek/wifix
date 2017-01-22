ed1ac47cecb6cecac2060e6eb85c589401552ee4
sageambrosek <sageisabeast@gmail.com>
2017-01-21 18:04:22 -0700

    Update wifix.py

--- wifix.py
+++ wifix.py
@@ -2,7 +2,7 @@
 import os
 response = os.system('ping 8.8.8.8 -c 1')
 if response == 0:
-	print 'exito'
+	print 'success'
 else:
 	print 'restarting network manager...'
 	os.system('sudo service network-manager restart')
